78b12064b41326769674652bab06ef28cbade344
TailerDerdan <105823298+TailerDerdan@users.noreply.github.com>
2024-07-19 11:31:13 +0300

    fixed

--- public/script/websocket/logicForStage.js
+++ public/script/websocket/logicForStage.js
@@ -4,9 +4,10 @@ import { webSocketObject } from "./lobby.js";
 export async function WaitingForPlayers(arrPlayersId) {
     if (webSocketObject.webSocket) {
         webSocketObject.webSocket.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            arrPlayersId.arr = data.arrPlayersId;
-            console.log(arrPlayersId.arr);
+            if (data.type === "waiting") {
+                const data = JSON.parse(event.data);
+                arrPlayersId.arr = data.arrPlayersId;
+            }
         };
     }
     else {
@@ -34,6 +35,31 @@ export async function SendPlayerId(arrPlayersId, idPlayer) {
         }
         webSocketObject.webSocket.send(JSON.stringify(data));
         MakePlayerReady();
+    }
+    else {
+        console.log("соединение разорвалось");
+    }
+}
+
+export async function SendBuilding() {
+    if (webSocketObject.webSocket) {
+        arrPlayersId.arr.push(idPlayer);
+        arrPlayersId.arr.sort();
+
+        let responseLobby = await fetch('/find_key_room', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        });
+        let lobbyData = await responseLobby.json();
+
+        const data = {
+            type: 'bulding',
+            arrPlayersId: arrPlayersId.arr,
+            key_room: lobbyData.key_room,
+        }
+        webSocketObject.webSocket.send(JSON.stringify(data));
     }
     else {
         console.log("соединение разорвалось");
